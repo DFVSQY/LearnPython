9e88d86dd14b3a6cb49ee6a4d94373e406555c52
tzj <tzj@example.com>
2022-09-12 20:15:09 +0800

    数的一些使用示例

--- src/main.py
+++ src/main.py
@@ -25,3 +25,26 @@ user_name = " bill jobs "
 print(user_name.lstrip())  # 移除开头空白
 print(user_name.rstrip())  # 移除结尾空白
 print(user_name.strip())  # 移除开头结尾空白
+
+# 两个乘号表示乘方
+print(3 ** 2)
+
+# 浮点数运算结果的小数位数可能是不确定的
+f = 0.2 + 0.1
+print(f)  # 0.30000000000000004
+
+# 除法运算中，结果总是浮点数
+print(4 / 2)  # 2.0
+
+# 其他运算中，如果一个操作数是整数，另一个是浮点数，结果也未浮点数
+print(3 + 2.0)  # 5.0
+print(3 * 2.0)  # 6.0
+print(3.0 ** 2)  # 9.0
+
+# 表示很大数时，可以用下划线分组便于阅读
+universe_age = 14_000_000_000
+print(universe_age)
+
+# 通常用全大写表示该变量为常量
+MATH_PI = 3.14
+print(MATH_PI)
