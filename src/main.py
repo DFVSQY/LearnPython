b2bd8917e31a292845f02e53b404061f23cb62af
DFVSQY <dfvsqy@qq.com>
2022-10-03 11:07:03 +0800

    字典中存储列表

--- src/main.py
+++ src/main.py
@@ -298,3 +298,9 @@ person_1 = {"name": "Bill", "age": 18, "sex": "man"}
 person_2 = {"name": "Job", "age": 23, "sex": "women"}
 persons = [person_1, person_2]
 print(persons)
+
+editor = {
+    "menus": ["file", "edit", "selection", "view", "go"],
+    "name": "visual studio code",
+}
+print(editor)
